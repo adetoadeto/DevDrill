1859c78a8841a86f49bbf5c917a8be4d07782230
adetoadeto <157330742+adetoadeto@users.noreply.github.com>
2025-11-01 12:26:36 +0100

    features updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,40 @@
 import "./header.css";
 import { Link } from "react-router-dom";
 
-function Header () {
-    return <nav>
-        <Link to="/" className="heading">DevDrill</Link>
+let signedIn = true;
+let admin = true;
+function Header() {
+  return (
+    <nav>
+      <Link to="/" className="heading">
+        DevDrill
+      </Link>
+      <ul>
+        <Link to="/">Home</Link>
+        <li>
+          <a href="/#overview-section">Overview</a>
+        </li>
+        <Link to="/play">Explore Games</Link>
+        {admin && <Link to="/admin"><i class="fa-solid fa-gauge"></i> Dashboard</Link> }
+      </ul>
+
+      {!signedIn ? (
+        <button className="small-button">
+          <Link to="/account">
+            Sign Up <i class="fa-solid fa-user-plus"></i>{" "}
+          </Link>
+        </button>
+      ) : (
         <ul>
-            <Link to="/">Home</Link>
-            <li><a href="/#overview-section">Overview</a></li>
-            <Link to="/play">Explore Games</Link>
-            <Link to="/user">Customer Dashboard</Link>
+            <Link to="/user"> Settings <i class="fa-solid fa-gear"></i></Link>
+            <button className="small-button">
+                <i class="fa-solid fa-arrow-right-from-bracket"></i> Log Out
+            </button>
         </ul>
-        <button className="small-button"><Link to="/account">Sign Up</Link></button>
+      )}
+     
     </nav>
+  );
 }
 
 export default Header;
